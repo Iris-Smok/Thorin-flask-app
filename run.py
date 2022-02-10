8467210e7465ff19345381dac08c683144aa3825
Iris-Smok <irissmok11@gmail.com>
2022-02-10 12:27:18 +0000

    Add json file and iterate over it

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 Flask
 """
 import os
+import json
 from flask import Flask, render_template  # Import the Flask class
 
 
@@ -23,7 +24,10 @@ def about():
     """
      About page
     """
-    return render_template("about.html", page_title="About")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
